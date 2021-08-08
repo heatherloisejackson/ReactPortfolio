a2065842d30ef716d77e4bfcd4f7160a4846ab13
Heather Jackson <hmjackson917@gmail.com>
2021-08-08 16:51:20 -0400

    add image paths as part of project data

--- src/components/06-Portfolio/index.js
+++ src/components/06-Portfolio/index.js
@@ -8,31 +8,36 @@ function Portfolio() {
       name: 'Bottom Drawer Marketplace',
       description: 'MongoDB/Express/Node/Handlebars',
       link: "http://bottomdrawermarketplace.herokuapp.com/",
-      repo: "https://github.com/heatherloisejackson/super-octo-funicular"
+      repo: "https://github.com/heatherloisejackson/super-octo-funicular",
+      screenshot: '../../assets/projects/bottomdrawer.png'
     },
     {
       name: 'Book Search Engine',
       description: 'MERN Stack',
       link: "https://heathers-google-book-search.herokuapp.com/",
-      repo: "https://github.com/heatherloisejackson/BookSearchEngine"
+      repo: "https://github.com/heatherloisejackson/BookSearchEngine",
+      screenshot: '../../assets/projects/booksearch.png'
     },
     {
       name: 'Flobi',
       description: 'Third Party APIs/HTML/JavaScript',
       link: "https://heatherloisejackson.github.io/Project-API/",
-      repo: "https://github.com/heatherloisejackson/Project-API"
+      repo: "https://github.com/heatherloisejackson/Project-API",
+      screenshot: '../../assets/projects/flobi.png'
     },
     {
       name: 'Fitness Tracker',
       description: 'MongoDB/Express/Node/HTML/CSS',
       link: "https://heathersfitnesstracker.herokuapp.com/?id=60ee2098fa027f0015fc91fe",
-      repo: "https://github.com/heatherloisejackson/FitnessTracker"
+      repo: "https://github.com/heatherloisejackson/FitnessTracker",
+      screenshot: '../../assets/projects/fitnesstracker.png'
     },
     {
       name: 'Team Profile Generator',
       description: 'Node.js/HTML/CSS',
       link: "https://github.com/heatherloisejackson/ProfessionalTeamProfileCreator",
-      repo: "https://github.com/heatherloisejackson/ProfessionalTeamProfileCreator"
+      repo: "https://github.com/heatherloisejackson/ProfessionalTeamProfileCreator",
+      screenshot: '../../assets/projects/teamprofilegenerator.png'
     },
   ]);
 
