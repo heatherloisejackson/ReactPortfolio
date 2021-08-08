8fa43bef3b8afd4694b387b9aefcbffb4eb1b8ef
Heather Jackson <hmjackson917@gmail.com>
2021-08-08 14:40:13 -0400

    nav created

---
+++ src/components/04-Nav/index.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+
+function Nav(props) {
+  const {
+    pages = [],
+    setCurrentPage,
+    currentPage,
+  } = props;
+
+  useEffect(() => {
+    document.title = currentPage.name;
+  }, [currentPage]);
+
+  return (    
+      <nav>
+        <ul className="flex-row">
+          {pages.map((Site) => (
+            <li
+              className={`mx-5 ${
+                currentPage.name === Site.name && 'navActive'
+                }`}
+              key={Site.name}
+            >
+              <span
+                onClick={() => setCurrentPage(Page)}
+              >
+                {Site.name}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </nav>
+  );
+}
+
+export default Nav;
