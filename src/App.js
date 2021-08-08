d1434bafaecbf86b22a9eaa6a56523af0d302996
Heather Jackson <hmjackson917@gmail.com>
2021-08-08 13:59:43 -0400

    new file structure to better organize components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import React from "react";
-import Portfolio from "./components/Portfolio";
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Website from "./components/Website";
+import Footer from "./components/Footer";
 
 function App() {
+  const [pages] = useState([
+    {
+      name: "about me"
+    },
+    { name: "portfolio" },
+    { name: "contact" },
+    {
+      name: "resume"
+    }
+  ]);
+
+  const [currentPage, setCurrentPage] = useState(pages[0]);
+
   return (
     <div>
-      <Portfolio />
+      <Header>
+        <Nav
+          pages={pages}
+          setCurrentPage={setCurrentPage}
+          currentPage={currentPage}
+        ></Nav>
+      </Header>
+      <main>
+        <Website currentPage={currentPage}></Website>
+      </main>
+      <Footer />
     </div>
   );
 }
