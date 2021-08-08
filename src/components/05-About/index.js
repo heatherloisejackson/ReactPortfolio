1e3bd9e77a526d214b0215ef0123194cb2b5e330
Heather Jackson <hmjackson917@gmail.com>
2021-08-08 14:47:40 -0400

    created about page

---
+++ src/components/05-About/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+function About() {
+  return (
+    <section className="my-5">
+      <div className="my-2">
+        <div className="profile-img my-5">
+          <i className="fas fa-user-circle" style={{ fontSize: "96px" }}></i>
+        </div>
+        <p>
+          Hi, I'm Heather. I'm on my way to becoming a Full-Stack Web Developer,
+          currently enrolled in University of Pennsylvania's Coding Bootcamp.
+        </p>
+        <p>
+          Soon I plan to retire from the food industry so I can dive into a full time
+          position in tech. In order to be well-rounded in this field, I'm
+          interested in a variety of positions, but I can't deny a special
+          interest in Front-End configuration. This August I will recieve my
+          Full-Stack Web Development Certificate and I am really excited to
+          continue learning while actually on the ground. My Coding experience
+          is recent, but intensive. I also have a Bachelor's Degree in Art
+          History and Criticism from Temple University, where I spent years
+          gaining experience constantly adapting to new enviornments with roles
+          in a variety of departments and organizations in the art world. More
+          recently, I've also been working with small businesses to build their
+          brands through various forms of marketing and gaining virtual foot
+          traffic by means of website design and e-commerce.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default About;
